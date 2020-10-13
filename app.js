15a4a81053094fc82f45943f9d50759281731d59
Eunbi Kim <kimeunb1822@gmail.com>
2020-10-13 14:45:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ var indexRouter = require("./routes/index");
 app.use("/", indexRouter);
 
 
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+  });
+  
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
 
 app.listen(process.env.PORT, ()=>{
     console.log(`http://localhost:${port}`);
