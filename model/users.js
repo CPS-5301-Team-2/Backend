73414c0c919fe5c2cebfa0b169d8d499868cde5c
Syed Nadeem <syedfnadeem323@gmail.com>
2020-10-19 19:56:25 -0400

    Added basic user model.

--- model/users.js
+++ model/users.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 var user = mongoose.Schema({
 
-    name: {type: String, required: true},
-    username: {type: String, required: true},
+    name: {type: String, required: true, maxlength: 50},
+    username: {type: String, required: true, maxlength: 10},
     password: {type: String, required: true},
     email: {type: String, required: true},
     position: {type: String, required: true},
