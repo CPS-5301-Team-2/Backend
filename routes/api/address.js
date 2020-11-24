19f3910d457db3b13d49b7328f4505a9b23bd4fc
Eunbi Kim <kimeunb1822@gmail.com>
2020-11-24 13:12:31 -0500

    parse response

--- routes/api/address.js
+++ routes/api/address.js
@@ -9,10 +9,35 @@ router.post("/locations", async(req,res)=>{
     
     try{
         const response = await got(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.678947,%20-74.232321&radius=500&type=restaurant&key=AIzaSyA5AtkBvlXeI567r7_tm3JlcYivAmfEmxs`);
-        console.log(response.body);
+
+        var obj = JSON.parse(response.body);
+
+        var arr = [];
+        for(i=0; i<obj.results.length; i++)
+        {
+            var temp_json = {
+                "business_status": obj.results[i].business_status,
+                "lat": obj.results[i].geometry.location.lat,
+                "lng": obj.results[i].geometry.location.lng,
+                "icon": obj.results[i].icon,
+                "types": obj.results[i].types,
+                "vicinity": obj.results[i].vicinity
+            };
+
+            arr.push(temp_json);
+        }
+
+        console.log(arr);
+
+        // //latitude
+        // console.log(obj.results[4].geometry.location.lat);
+
+
     }catch (error){
         console.log(error.response.body);
     }
+
+
 });
 
 
