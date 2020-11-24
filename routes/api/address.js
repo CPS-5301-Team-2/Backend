0f9518d91c3cfa2277ea8fd948aa3e63e843d7cd
Eunbi Kim <kimeunb1822@gmail.com>
2020-11-24 13:33:17 -0500

    parse mapinfo
    add name
    render to homepage

--- routes/api/address.js
+++ routes/api/address.js
@@ -9,10 +9,9 @@ router.post("/locations", async(req,res)=>{
     
     try{
         const response = await got(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.678947,%20-74.232321&radius=500&type=restaurant&key=AIzaSyA5AtkBvlXeI567r7_tm3JlcYivAmfEmxs`);
-
         var obj = JSON.parse(response.body);
 
-        var arr = [];
+        var maparr = [];
         for(i=0; i<obj.results.length; i++)
         {
             var temp_json = {
@@ -20,24 +19,20 @@ router.post("/locations", async(req,res)=>{
                 "lat": obj.results[i].geometry.location.lat,
                 "lng": obj.results[i].geometry.location.lng,
                 "icon": obj.results[i].icon,
+                "name": obj.results[i].name,
                 "types": obj.results[i].types,
                 "vicinity": obj.results[i].vicinity
             };
-
-            arr.push(temp_json);
+            maparr.push(temp_json);
         }
 
-        console.log(arr);
-
-        // //latitude
-        // console.log(obj.results[4].geometry.location.lat);
-
+        res.render("homepage", {
+            mapinfo = maparr
+        });
 
     }catch (error){
         console.log(error.response.body);
     }
-
-
 });
 
 
