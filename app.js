f893e0e64c0aaddadaac29a9b619695e847ad16e
Syed Nadeem <syedfnadeem323@gmail.com>
2020-12-05 10:06:38 -0500

    Fixed download not found error

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ var adminList = require("./routes/api/listget");
 var auth = require("./routes/auth.js");
 var update = require("./routes/update");
 var location = require("./routes/api/address");
-var download = require("./routes/api/download");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -51,8 +50,6 @@ app.use("/list", adminList);
 app.use("/auth", auth);
 app.use("/update", update);
 app.use("/location",location);
-app.use("/download", download);
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
